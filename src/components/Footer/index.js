671458817f628cf008659363ff85ded3dd715513
Juraev Ozodbek <ozodbekmcfly@gmail.com>
2021-05-17 12:23:28 +0500

    First Commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter, FaYoutube } from 'react-icons/fa';
+import { 
+  FooterContainer, 
+  FooterWrap,
+  SocialMedia,
+  SocialMediaWrap,
+  SocialLogo,
+  SocialIcons,
+  SocialIconLink
+} from './FooterElements';
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterWrap>
+        <SocialMedia>
+          <SocialMediaWrap>
+            <SocialLogo to='/'>Pizza</SocialLogo>
+            <SocialIcons>
+              <SocialIconLink href='/' traget='_blank' aria-label='Facebook' rel='noopener noreferrer'>
+                <FaFacebook />
+              </SocialIconLink>
+              <SocialIconLink href='/' traget='_blank' aria-label='Instagram' rel='noopener noreferrer'>
+                <FaInstagram />
+              </SocialIconLink>
+              <SocialIconLink href='/' traget='_blank' aria-label='Youtube' rel='noopener noreferrer'>
+                <FaYoutube />
+              </SocialIconLink>
+              <SocialIconLink href='/' traget='_blank' aria-label='Twitter' rel='noopener noreferrer'>
+                <FaTwitter />
+              </SocialIconLink>
+              <SocialIconLink href='/' traget='_blank' aria-label='Linkedin' rel='noopener noreferrer'>
+                <FaLinkedin />
+              </SocialIconLink>
+            </SocialIcons>
+          </SocialMediaWrap>
+        </SocialMedia>
+      </FooterWrap>
+    </FooterContainer>
+  )
+}
+
+export default Footer
---
+++ src/components/Products/data.js
@@ -0,0 +1,64 @@
+import product1 from '../../images/product-1.jpg';
+import product2 from '../../images/product-2.jpg';
+import product3 from '../../images/product-3.jpg';
+import sweet1 from '../../images/sweet3.jpg';
+import sweet2 from '../../images/sweet-2.jpg';
+import sweet3 from '../../images/sweet-3.jpg';
+
+export const productData = [
+  {
+    img: product1,
+    alt: 'Pizza',
+    name: 'Supreme Pizza',
+    desc: 'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+    price: '$19.19',
+    button: 'Add to Cart'
+  },
+  {
+    img: product2,
+    alt: 'Pizza',
+    name: 'Hawaiian Paradise',
+    desc: 'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+    price: '$16.19',
+    button: 'Add to Cart'
+  },
+  {
+    img: product3,
+    alt: 'Pizza',
+    name: 'Veggie Overload',
+    desc: 'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+    price: '$14.19',
+    button: 'Add to Cart'
+  }
+];
+
+export const productDataTwo = [
+  {
+    img: sweet1,
+    alt: 'Pizza',
+    name: 'Supreme Pizza',
+    desc: 'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+    price: '$19.19',
+    button: 'Add to Cart'
+  },
+  {
+    img: sweet2,
+    alt: 'Pizza',
+    name: 'Hawaiian Paradise',
+    desc: 'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+    price: '$16.19',
+    button: 'Add to Cart'
+  },
+  {
+    img: sweet3,
+    alt: 'Pizza',
+    name: 'Veggie Overload',
+    desc: 'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+    price: '$14.19',
+    button: 'Add to Cart'
+  }
+]
+
+
+// export default productData;
+// export default productDataTwo;
